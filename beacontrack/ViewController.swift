f609f993fa57a84f75e41eb1cce7e95bc9cdcc69
Vivek <vivekpatna52@gmail.com>
2016-01-09 15:08:54 +0530

    Initial Commit

---
+++ beacontrack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  beacontrack
+//
+//  Created by vivek vivek on 09/01/16.
+//  Copyright (c) 2016 inkoop.in. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
